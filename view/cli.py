a8b943f9f696818db86a05db8f73add6638b87b4
PE4R <93002406+PE4R@users.noreply.github.com>
2021-12-18 19:33:17 +0000

    Início da função comando

--- view/cli.py
+++ view/cli.py
@@ -1,7 +1,25 @@
 import controller
 
-def comando():
-    pass
+def comando(instrucao):
+    if instrucao[0]=='RJ': #Registar Jogador
+        pass
+    elif instrucao[0]=='LJ': #Listar Jogadores
+        pass
+    elif instrucao[0]=='IJ': #Iniciar Jogo
+        pass
+    elif instrucao[0]=='IJA': #Iniciar Jogo Automático
+        pass
+    elif instrucao[0]=='DJ': #Detalhes de Jogo
+        pass
+    elif instrucao[0]=='J': #Efetuar Jogada
+        pass
+    elif instrucao[0]=='D': #Desistir de Jogo
+        pass
+    elif instrucao[0]=='G': #Gravar
+        pass
+    elif instrucao[0]=='L': #Ler
+        pass
+    else: return None
 
 def main():
     pass
