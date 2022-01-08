e112eac6b83f8df3e2bed19d4b0c2495398985bc
PE4R <93002406+PE4R@users.noreply.github.com>
2022-01-08 23:11:12 +0000

    Remoção de redundâncias nos returns

--- cli.py
+++ cli.py
@@ -2,46 +2,25 @@ import controller
 
 def comando(instrucao):
     if instrucao[0]=='RJ' and len(instrucao)==2: #Registar Jogador (RJ NomeJogador)
-        resultado = controller.registar(instrucao[1])
-        if resultado == None:
-            return "Jogador existente."
-        else: return "Jogador registado com sucesso."
+        return controller.registar(instrucao[1])
     
     elif instrucao[0]=='LJ' and len(instrucao)==1: #Listar Jogadores (LJ)
-        resultado = controller.listar()
-        if resultado == None:
-            return "Sem jogadores registados."
-        else: return resultado
+        return controller.listar()
 
     elif instrucao[0]=='IJ' and len(instrucao)==3: #Iniciar Jogo (IJ NomeJogadorA NomeJogadorB)
-        resultado = controller.iniciar(instrucao[1], instrucao[2])
-        if resultado == None:
-            return "Jogador inexistente."
-        elif resultado == False:
-            return "Existe um jogo em curso."
-        else: return "Jogo iniciado com sucesso."
+        return controller.iniciar(instrucao[1], instrucao[2])
 
     elif instrucao[0]=='IJA' and len(instrucao)==3: #Iniciar Jogo Automático (IJA NomeJogador Nível)
         pass
 
     elif instrucao[0]=='DJ' and len(instrucao)==1: #Detalhes de Jogo (DJ)
-        resultado = controller.detalhes()
-        if resultado == None:
-            return "Não existe jogo em curso."
-        else: return resultado
+        return controller.detalhes()
 
     elif instrucao[0]=='J' and len(instrucao)==3: #Efetuar Jogada (J NomeJogador Posição)
         pass
 
     elif instrucao[0]=='D' and len(instrucao)>1 and len(instrucao)<=3: #Desistir de Jogo (D NomeJogador NomeJogador)
-        resultado = controller.desistir(instrucao)
-        if resultado == None:
-            return "Não existe jogo em curso."
-        elif resultado == True:
-            return "Jogador inexistente."
-        elif resultado == False:
-            return "Jogador não participa no jogo em curso."
-        else: return resultado
+        return controller.desistir(instrucao)
 
     elif instrucao[0]=='G' and len(instrucao)==2: #Gravar (G NomeFicheiro)
         pass
--- controller.py
+++ controller.py
@@ -1,25 +1,25 @@
 import model
 
-def registar(player): #Adiciona e retorna True quando o jogador não existe, retorna None quando o jogador existe
-    for i in range(len(model.jogadores)):
+def registar(player): 
+    for i in range(len(model.jogadores)): #Verifica se o Jogador já está registado
         if player == model.jogadores[i].get('Jogador'):
-            return None #Já existe
-    model.jogadores.append({'Jogador': player, 'Jogos': 0, 'Vitorias': 0, 'Empates': 0, 'Derrotas': 0})
-    return True #Sucesso
+            return "Jogador existente."
+    model.jogadores.append({'Jogador': player, 'Jogos': 0, 'Vitorias': 0, 'Empates': 0, 'Derrotas': 0}) #Regista o Jogador
+    return "Jogador registado com sucesso."
 
 def listar(): #Retorna os valores das chaves 'Jogador' de cada dicionário na lista jogadores
     resultado = ""
     if len(model.jogadores)==0:
-        return None #Sem jogadores
-    else:
-        for i in range(len(model.jogadores)):
+        return "Sem jogadores registados."
+    else: #Organiza uma string com a scoreboard
+        for i in range(len(model.jogadores)): 
             resultado += model.jogadores[i].get('Jogador') 
             resultado += (" ") + str(model.jogadores[i].get('Jogos'))
             resultado += (" ") + str(model.jogadores[i].get('Vitorias'))
             resultado += (" ") + str(model.jogadores[i].get('Empates'))
             resultado += (" ") + str(model.jogadores[i].get('Derrotas'))
             if i != len(model.jogadores)-1:
-                resultado += ("\n")   
+                resultado += ("\n") #Adiciona newline entre jogadores, de modo a não adicionar depois do último
         return resultado #Sucesso, lista de jogadores
 
 def iniciar(jogador_a, jogador_b): #Inicia um novo jogo entre dois jogadores e retorna a saída com sucesso ou insucesso
@@ -32,18 +32,18 @@ def iniciar(jogador_a, jogador_b): #Inicia um novo jogo entre dois jogadores e r
             flag += 1
     if flag == 2:
         if model.jogo != {}:
-            return False #Jogo em curso
+            return "Existe um jogo em curso."
         model.jogo.update({
         'JogadorA': jogador_a, 'A1':4, 'A2':4, 'A3':4, 'A4':4, 'A5':4, 'A6':4, 'A7':0, 
         'JogadorB': jogador_b, 'B1':4, 'B2':4, 'B3':4, 'B4':4, 'B5':4, 'B6':4, 'B7':0})
-        return True #Sucesso
-    else: return None #Jogador Inexistente
+        return "Jogo iniciado com sucesso."
+    else: return "Jogador inexistente."
 
 def detalhes(): #Mostra o tabuleiro do jogo em curso
     if model.jogo == {}:
-        return None #Não existe jogo em curso
+        return "Não existe jogo em curso."
     else:
-        lista = list(model.jogo.values())
+        lista = list(model.jogo.values()) #Recolhe os valores do dicionário do jogo para dentro de uma lista
         resultado = str(lista[0])
         for i in range(6):
             resultado = resultado + ' [' + str(lista[i+1]) + ']'
@@ -51,19 +51,19 @@ def detalhes(): #Mostra o tabuleiro do jogo em curso
         for i in range(6):
             resultado = resultado + ' [' + str(lista[i+9]) + ']'
         resultado = resultado + ' (' + str(lista[15]) + ')'
-        return resultado #Sucesso
+        return resultado #Sucesso, retorna uma string com os valores estruturados
 
 def desistir(instrucao): #Mostra o tabuleiro do jogo em curso
     if model.jogo == {}:
-        return None #Não existe jogo em curso
+        return "Não existe jogo em curso."
     else:
         instrucao.remove('D')
         if len(instrucao) == 1: #1 jogador
             if instrucao[0] != model.jogo.get('JogadorA') and instrucao[0] != model.jogo.get('JogadorB'):
                 for i in range(len(model.jogadores)):
                     if model.jogadores[i].get('Jogador') == instrucao[0]:
-                        return False #Jogador não participa no jogo em curso.
-                return True #Jogador inexistente.
+                        return "Jogador não participa no jogo em curso."
+                return "Jogador inexistente."
             else:
                 for i in range(len(model.jogadores)):
                     if model.jogadores[i].get('Jogador') == instrucao[0]:
@@ -93,11 +93,11 @@ def desistir(instrucao): #Mostra o tabuleiro do jogo em curso
                     if model.jogadores[i].get('Jogador') == instrucao[0]: #Se o primeiro existe
                         flag = True #Primeiro existe
                         break
-                if flag == False: return True #Se o primeiro não existe, Jogador inexistente
+                if flag == False: return "Jogador inexistente." #Se o primeiro não existe
                 for i in range(len(model.jogadores)):
                     if model.jogadores[i].get('Jogador') == instrucao[1]: #Se o segundo existe
-                        return False #Ambos existem: Jogador não participa no jogo em curso.
-                return True #O segundo não existe: Jogador inexistente.
+                        return "Jogador não participa no jogo em curso." #Ambos existem
+                return "Jogador inexistente." #O segundo não existe
             else:
                 for i in range(len(model.jogadores)):
                     if model.jogadores[i].get('Jogador') == instrucao[0]:
